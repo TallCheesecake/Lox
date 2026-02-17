6badcac90028a5be7884b3a4f7a147218e8d8f4e
bigThinkpad <example@example.com>
2026-02-16 22:14:26 -0500

    readme update

---
+++ src/exe.rs
@@ -0,0 +1,16 @@
+use crate::parser::{Atom, Tree};
+
+pub fn print_execute(input: Tree) {
+    match input {
+        Tree::Nil => todo!(),
+        Tree::Call { callee, arguments } => todo!(),
+        Tree::Atom(atom) => match atom {
+            Atom::String(x) => {
+                println!("{x}");
+            }
+            _ => {}
+        },
+        Tree::NonTerm(op, trees) => todo!(),
+        Tree::Op(op) => todo!(),
+    }
+}
